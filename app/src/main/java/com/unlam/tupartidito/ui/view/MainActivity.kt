cdd5c587efb5b80be11bbf8792b4d069111dce59
rao <oar.adrian@gmail.com>
2021-10-13 21:16:44 -0300

    commit inicial

---
+++ app/src/main/java/com/unlam/tupartidito/ui/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.unlam.tupartidito.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unlam.tupartidito.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
